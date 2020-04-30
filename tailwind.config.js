ab5efd41a50cf66e5818ab694d0743fb0f4203e6
Marc Stammerjohann <marcstammerjohann@web.de>
2020-04-30 16:40:42 +0200

    chore: update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         background: 'var(--background)',
         color: 'var(--text-color)',
         'color-light': 'var(--text-color-light)',
-        tabs: 'var(--tabs)'
+        tabs: 'var(--tabs)',
       },
     },
     screens: {
