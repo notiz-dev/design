af8a16041aeb9a2f5c89338f86e6894c748ff5a2
Marc Stammerjohann <marcstammerjohann@web.de>
2020-05-18 18:23:04 +0200

    feat(chip): increase padding x and decrease margin

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  tags: string[] = ['Git', 'Quick Tip', 'Angular', 'Nest', 'Prisma'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
